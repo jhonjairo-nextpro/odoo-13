b4ba39d2dd3f62929faed52bfa3edcdcd7d83d0c
Jhon Jairo Rojas Ortiz <jhon.rojas@nextpro.pe>
2022-03-28 21:17:10 -0500

    .

--- payment_paymentez/models/sale_order.py
+++ payment_paymentez/models/sale_order.py
@@ -6,3 +6,4 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
     paymentez_auth_code = fields.Char(string='Numero de autorización de pago')
     paymentez_trans_code = fields.Char(string='ID de transacción de pago')
+    is_quote = fields.Boolean(string='Es cotización?')
